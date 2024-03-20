ce85912a7c853c120018969a2f1c5fc30bcbddaf
shimtaewook <to1583@naver.com>
2024-03-20 15:53:07 +0900

    2024-03-20 15:50

--- src/main/java/com/taewook/basic/controller/RequestDataController.java
+++ src/main/java/com/taewook/basic/controller/RequestDataController.java
@@ -1,6 +1,9 @@
 package com.taewook.basic.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +20,53 @@ public class RequestDataController {
 
     // HTTP GET localhost:4000/request-data/request-param
     @GetMapping("/request-param")
+
     // http://localhost:4000/request-data/request-param?userId=qwer&userName=gildong
     public String getRequestParam(
-        @RequestParam(name="userId") String userId,
-        @RequestParam(name="userName") String userName
+        @RequestParam(name="userId") String userId, // 무조건 받아야 하는 방식
+        @RequestParam(name="userName", required = false) String userName,  // required = false로 지정하면 필수로 입력 하지 않아도 됨, 무조건 받지 않아도 되는 방식
+        @RequestParam() int userAge // 좋지 않은 방식
     ) {
-        return "사용자 아이디 : " + userId + "사용자 이름 : " + userName;
+        return "사용자 아이디 : " + userId + " / 사용자 이름 : " + userName + " / 사용자 나이:" + userAge;
+    }
+
+    // @PathVariable() :
+    // - 모든 HTTP 메서드에서 URL의 특정 패턴에 따라서 데이터를 추출하는 방식
+
+    // HTTP DELETE localhost:4000/request-data/path-variable
+    @DeleteMapping("/path-variable/{age}")
+
+    // http://localhost:4000/request-data/path-variable/10
+    public String deletePathVariable(
+        @PathVariable("age" ) Integer age
+    ){
+        return "사용자 나이 : " + age;
+    }
+
+    // HTTP PATCH localhost:4000/request-data/patch/gildong/update
+    @PatchMapping("/patch/{userName}/update")
+
+    // http://localhost:4000/request-data/patch/gildong/update
+    public String patchUpdate (
+        @PathVariable("userName") String userName
+    ){
+        return "사용자 이름 : " + userName;
+    }
+
+    //! *****주의
+    // URL 패턴으로 데이터를 받아오는 방식을 썼을 때 겹치는 패턴이 존재하는지 잘 확인 해야 함
+    //! *****주의
+    @GetMapping("/{value}/get")
+    public String getPathVariable1(
+        @PathVariable("value") String value
+    ){
+        return "getPathVariable1";
+    }
+    
+    @GetMapping("/get/{value}")
+    public String getPathVariable2(
+        @PathVariable("value") String value
+    ){
+        return "getPathVariable2";
     }
 }

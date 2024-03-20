4b8dc48af1646df5634199a8a594a046d9d50f70
shimtaewook <to1583@naver.com>
2024-03-20 16:51:09 +0900

    2024-03-20 16:50

--- src/main/java/com/taewook/basic/controller/RequestDataController.java
+++ src/main/java/com/taewook/basic/controller/RequestDataController.java
@@ -4,9 +4,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.taewook.basic.dto.SampleDto;
 
 @RestController
 
@@ -15,8 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RequestDataController {
 
     // @RequestParam() :
-    // - GET, DELETE 처럼 URL로 데이터를 전송하는 메서드에서 Query String 으로 지정된
-    //    데이터를 가져오기 위해 사용
+    // - GET, DELETE 처럼 URL로 데이터를 전송하는 메서드에서 Query String 으로 지정된 데이터를 가져오기 위해 사용
 
     // HTTP GET localhost:4000/request-data/request-param
     @GetMapping("/request-param")
@@ -34,13 +37,18 @@ public class RequestDataController {
     // - 모든 HTTP 메서드에서 URL의 특정 패턴에 따라서 데이터를 추출하는 방식
 
     // HTTP DELETE localhost:4000/request-data/path-variable
-    @DeleteMapping("/path-variable/{age}")
+    @DeleteMapping({    
+        "/path-variable/{age}",     //url 중복 지정 하는법 ({ "url1", "url2", ...})
+        "/path-variable/{age}/{name}",
+        "/path-variable/{age}/"
+    })
 
-    // http://localhost:4000/request-data/path-variable/10
+    // http://localhost:4000/request-data/path-variable/10/gilldong
     public String deletePathVariable(
-        @PathVariable("age" ) Integer age
+        @PathVariable("age" ) Integer age,
+        @PathVariable(name = "name", required = false) String name
     ){
-        return "사용자 나이 : " + age;
+        return "사용자 나이 : " + age + " / 사용자 이름 : " + name;
     }
 
     // HTTP PATCH localhost:4000/request-data/patch/gildong/update
@@ -69,4 +77,17 @@ public class RequestDataController {
     ){
         return "getPathVariable2";
     }
+
+    // @RequestBody() :
+    // - POST, PATCH, PUT 처럼 RequestBody로 데이터를 전송하는 메서드에서 데이터를 가져오기 위해 사용
+
+    // HTTP POST localhost:4000/request-data/post
+    @PostMapping("/post")
+    public String post(
+        // @RequestBody String text
+        @RequestBody SampleDto dto
+    ){
+        return "전송한 데이터 : " + dto.toString();
+    }
+
 }
